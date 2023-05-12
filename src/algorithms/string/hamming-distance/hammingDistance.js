b2d25139e1e18be5afc851330c3110628d093a31
Brandon Lowe <BLoweCLE@gmail.com>
2023-05-12 11:24:37 -0500

    hammingDistance

--- src/algorithms/string/hamming-distance/hammingDistance.js
+++ src/algorithms/string/hamming-distance/hammingDistance.js
@@ -3,18 +3,17 @@
  * @param {string} b
  * @return {number}
  */
-export default function hammingDistance(a, b) {
-  if (a.length !== b.length) {
-    throw new Error('Strings must be of the same length');
-  }
 
-  let distance = 0;
-
-  for (let i = 0; i < a.length; i += 1) {
-    if (a[i] !== b[i]) {
-      distance += 1;
+export default function hammingDistance(str1, str2) {
+    if(str1.length !== str2.length){
+        throw new Error("strings must be the same length")
     }
-  }
 
-  return distance;
+    let count = 0;
+    
+    for(let i=0; i < str1.length; i++){
+        if(str1[i] !== str2[i]) count++
+    }
+
+    return count;
 }
