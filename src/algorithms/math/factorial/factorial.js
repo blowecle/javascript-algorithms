73370c3269a725310ee811af92a5c21222a6bf37
Brandon Lowe <BLoweCLE@gmail.com>
2023-05-14 08:40:38 -0500

    factorial

--- src/algorithms/math/factorial/factorial.js
+++ src/algorithms/math/factorial/factorial.js
@@ -5,9 +5,9 @@
 export default function factorial(number) {
   let result = 1;
 
-  for (let i = 2; i <= number; i += 1) {
+  for(let i = 2; i <= number; i++){
     result *= i;
   }
-
+  
   return result;
 }
